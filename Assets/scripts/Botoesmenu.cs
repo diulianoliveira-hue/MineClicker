20cb3029a9d479f3d23ec3b0054f1fa62100b420
Diulian <Diulian_oliveira@estudante.sesisenai.org.br>
2025-12-01 17:16:59 -0300

    commit inicial

---
+++ Assets/scripts/Botoesmenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Botoesmenu : MonoBehaviour
+
+  
+{
+	public void IniciarJogo()
+	{
+
+		SceneManager.LoadScene("Jogo");
+	}
+}
